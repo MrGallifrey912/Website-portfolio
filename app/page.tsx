3e332f64fb69d72e79a0a0b107a491aa5c067ba0
Andrew Jones <andrewjones.techguy@gmail.com>
2024-02-26 01:21:48 -0800

    456

--- app/page.tsx
+++ app/page.tsx
@@ -24,12 +24,12 @@ export default function Home() {
 
       {/** Resume button */}
       <div className="flex justify-center">
-      <a 
+      <Link 
         className="bg-green-800 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded"
         download="Resume.pdf"
         href="/Resume.pdf">  
         Resume
-      </a> 
+      </Link> 
     </div>
 
       
