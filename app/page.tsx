909ea8faab1539908d3709733f4f49caf569a802
Andrew Jones <andrewjones.techguy@gmail.com>
2024-02-26 14:08:38 -0800

    attempting to fix the resume btn now

--- app/page.tsx
+++ app/page.tsx
@@ -24,12 +24,12 @@ export default function Home() {
 
       {/** Resume button */}
       <div className="flex justify-center">
-      <Link 
+      <a 
         className="bg-green-800 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded mt-16 mb-16"
         download="Resume.pdf"
         href="/Resume.pdf">  
         Resume
-      </Link> 
+      </a> 
     </div>
 
       
