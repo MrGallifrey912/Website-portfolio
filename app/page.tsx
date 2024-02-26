26cc8a3b7e855a17e9d0d77da09be6dd7d66bf7c
Andrew Jones <andrewjones.techguy@gmail.com>
2024-02-26 14:21:02 -0800

    rel path for resume test

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       <div className="flex justify-center">
       <a 
         className="bg-green-800 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded mt-16 mb-16"
-        href="/Resume.pdf"
+        href="resume.pdf"
         download="Andrew-Jones.pdf">  
         Resume
       </a> 
