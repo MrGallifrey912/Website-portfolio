95c9bbfed16d6a93704ddcd5d353c7eacc9053fb
Andrew Jones <andrewjones.techguy@gmail.com>
2024-03-01 12:21:32 -0800

    margin btn adjust

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         Resume
       </a> 
       <a 
-        className="contact-btn hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded mt-16 mb-16"
+        className="contact-btn hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded mt-16 mb-16 ml-16"
         href="/forms"
         >  
         Contact
