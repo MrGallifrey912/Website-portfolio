9c695176a66f89a4b30ab08015980181c278af14
Andrew Jones <andrewjones.techguy@gmail.com>
2024-03-01 11:56:04 -0800

    btn assignment

--- app/page.tsx
+++ app/page.tsx
@@ -32,8 +32,8 @@ export default function Home() {
       </a> 
       <a 
         className="contact-btn hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded mt-16 mb-16"
-        href="resume.pdf"
-        download="Andrew-Jones.pdf">  
+        href="/forms"
+        >  
         Contact
       </a>
     </div>
@@ -43,7 +43,7 @@ export default function Home() {
 {/** Start of card buttons */}
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link
-          href="/forms"
+          href="#"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 m-4"
           target="_blank"
           rel="noopener noreferrer"
