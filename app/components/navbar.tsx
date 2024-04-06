9ee9322e795c6ae611fd2e4e785b42d4cd0ebf88
Andrew Jones <andrewjones.techguy@gmail.com>
2024-04-05 23:02:29 -0700

    fix: nav comp syntax and fix gitignore

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,14 +7,15 @@ export default function Navigation() {
     <div className={"flex justify-center mt-0"}>{/** Nav Container */}
     <div className={"flex fixed bg-gray-900 rounded-2xl p-2 m-12 mt-0 justify-evenly w-[600px]"}>{/** UL Container */}
     <img className="w-10 h-10 relative rounded-[32px] border border-black justify-start" src="/IMG_2649.jpg" />{/** Nav img */}
-      <ul className={"flex flex-row justify-items-center"}>{/** Nav index */}
+      <ul className={"flex flex-row justify-items-center"}>
         <li className={"mr-6"}><Link href="/" className={"text-green-50 hover:text-green-400"}>Home</Link></li>
         <li className={"mr-6"}><Link href="/about" className={"text-green-50 hover:text-green-400"}>About</Link></li>
         <li className={"mr-6"}><Link href="/projects" className={"text-green-50 hover:text-green-400"} style={{pointerEvents: "none"}}>Projects</Link></li>
         <li className={"mr-6"}><Link href="/blog" className={"text-green-50 hover:text-green-400"} style={{pointerEvents: "none"}}>Blog</Link></li>
       </ul>
+      {/** Add user logic here */}
       <Link
-      className='w-8 h-8 bg-gray-500'{/** Add user logic here*/}
+      className="w-8 h-8 bg-gray-500"
       href="/email-signup">
       </Link>
     </div>
